7a60d378217c930d5c79b39211291e7187de9c80
abdelghanyMh <wboductqr@relay.firefox.com>
2022-01-08 19:38:31 +0100

    add nav menu + home page

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+const menuBtn = document.querySelector(".menu-btn");
+const humburger = document.querySelector(".menu-btn__burger");
+
+// used to display nav menu 
+const menuNav = document.querySelector(".menu-nav");
+
+// used to display nav menu item  (LINKS)
+const menuNavItems = document.querySelectorAll(".menu-nav__item");
+
+
+
+
+// add toggle functionalty to humburger button
+function toggleMenu() {
+    if (humburger.classList.contains('open')) {
+        humburger.classList.remove('open')
+
+        menuNav.classList.remove('open')
+        
+        menuNavItems.forEach(item => {
+            item.classList.remove('open')
+        })
+
+    } else {
+        humburger.classList.add('open')
+        menuNav.classList.add('open')
+
+        menuNavItems.forEach(item => {
+            item.classList.add('open')
+        })
+    }
+}
+// event listeners
+menuBtn.addEventListener('click', toggleMenu)
